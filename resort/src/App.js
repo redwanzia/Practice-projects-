397577a043b26b281659cd6bf66067183b4475ef
Redwan Zia <redwan.zia@gmail.com>
2020-01-24 08:55:45 -0500

    Added Navbar component

--- resort/src/App.js
+++ resort/src/App.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react'
+
+import {Route,Switch} from 'react-router-dom'
+
+
 import './App.css'
 import Home from './pages/Home'
 import Rooms from './pages/Rooms'
 import SingleRoomPage from './pages/SingleRoomPage'
 import ErrorPage from './pages/Error'
 
-import {Route,Switch} from 'react-router-dom'
+
+import NavBar from './components/NavBar'
+
+
 
 
  class App extends Component {
   render() {
     return (
       <div>
-
+      <NavBar/>
+      <Switch>      
       <Route exact path='/' component={Home}/>
       <Route exact path='/rooms' component={Rooms}/>
       <Route exact path='/rooms/:slug' component={SingleRoomPage}/>
+      <Route component ={ErrorPage}/>
+      </Switch>
 
 
       </div>
