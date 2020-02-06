1793014c197350a22e4f827036ab899d601cd1b2
Redwan Zia <redwan.zia@gmail.com>
2020-02-05 23:17:01 -0500

    added use state hook and accumulated number with reduce

--- budget-hooks-context/src/App.js
+++ budget-hooks-context/src/App.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ExpenseList from './components/ExpenseList'
 import ExpenseForm from './components/ExpenseForm'
 import Alert from './components/Alert'
-
+import uuid from 'uuid'
 import './App.css';
 
+
+const initialExpenses = [
+
+  {id : uuid(), charge: 'rent', amount :1600 },
+  {id : uuid(), charge: 'car payment', amount :400 },
+  {id : uuid(), charge: 'credit card', amount :1200 }
+
+]
+
 function App() {
+  
+  const [expenses,setExpenses] = useState(initialExpenses)  
+  
+
+
   return (
-    <div >      
+    <div >  
+        
       <Alert/>
-      <ExpenseForm/>
-      <ExpenseList/>
-      
+      <h1>Budget Calculator</h1>
+      <main className='App' >
+        <ExpenseForm/>
+        <ExpenseList expenses={expenses} />
+      </main>
+      <h1>
+        Total Spending: <span className='total' >
+        ${expenses.reduce((acc,current) =>{
+
+          return acc += current.amount
+
+
+        },0)}
+        </span>
+      </h1>
+
     </div>
   );
 }
--- budget-hooks-context/src/components/ExpenseList.js
+++ budget-hooks-context/src/components/ExpenseList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ExpenseItem from './ExpenseItem'
+
 const ExpenseList = () => {
   return (
     <div>
